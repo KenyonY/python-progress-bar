90d80c5b02782d4acc25d45d27f5e1a308d25de6
k.y <kunyuany@163.com>
2020-06-06 23:17:46 +0800

    Daily update

--- examples.py
+++ examples.py
@@ -9,23 +9,6 @@ s2 = rgb_str("I'm green", [0, 255, 0])
 def test_rgb_string():
     print(s1, s2)
 
-def test_performance():
-    from tqdm import tqdm
-    N = 20000000
-    print("probar:")
-    for i in probar(range(N), color='1', time_interval=0.02):
-        pass
-
-    print('bar:')
-    for idx, i in enumerate(range(N)):
-        bar(idx, N, text=f'{idx+1}', color='5')
-
-    print("tqdm:")
-    for i in tqdm(range(N)):
-        pass
-
-test_performance()
-
 def test_probar1():
     for idx, i in probar(range(1234), enum=True):
         time.sleep(0.0061)
@@ -45,6 +28,22 @@ def test_custom_color():
         bar(idx, 1234, text=s2, color=color)
         time.sleep(0.005)
 
+def test_performance():
+    from tqdm import tqdm
+    N = 10000000
+    print("probar:")
+    for i in probar(range(N), color='1', time_interval=0.02):
+        pass
+    time.sleep(0.5)
+    
+    print('bar:')
+    for idx, i in enumerate(range(N)):
+        bar(idx, N, text=f'{idx+1}', color='5')
 
+    print("tqdm:")
+    for i in tqdm(range(N)):
+        pass
+
+test_performance()
 
 
