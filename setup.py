8fa3588ab33e84b095c66fc6d4451efde9b48504
k.y <kunyuany@163.com>
2020-06-24 00:38:30 +0800

    more accurate description

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
- # -*- coding:utf-8 -*-
+# -*- coding:utf-8 -*-
 from __future__ import print_function
 from setuptools import setup, find_packages
 from glob import glob
@@ -11,38 +11,29 @@ install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
 with open("README.md", "r", encoding='utf-8') as fr:
     long_description = fr.read()
 
-
-setup(
-       name = pyprobar.__name__ ,
-       version=pyprobar.__version__,
-       packages = find_packages(),
-       include_package_data = True,
-       description = " An easy-to-use and colorful progress bar for python." ,
-
-       long_description=long_description,
-       long_description_content_type="text/markdown",
-       author = "K.y" ,
-       author_email="beidongjiedeguang@gmail.com",
-       url = "https://github.com/beidongjiedeguang/python-progress-bar" ,
-       license = "MIT" ,
-
-       install_requires=install_requires,
-       classifiers=[
-                    'Operating System :: OS Independent',
-                    'Intended Audience :: Developers',
-                    'Intended Audience :: Science/Research',
-                    'Topic :: Scientific/Engineering :: Artificial Intelligence',
-                    "Programming Language :: Python :: 3",
-                    "Programming Language :: Python :: 3.5",
-                    "Programming Language :: Python :: 3.6",
-                    "Programming Language :: Python :: 3.7",
-                    "Programming Language :: Python :: 3.8",
-       ],
-       keywords=[
-          'Python Utils',
-          'Machine Learning',
-          'Neural Networks',
-          'Natural Language Processing',
-          'Computer Vision'
-      ]
-)
+setup(name=pyprobar.__name__,
+      version=pyprobar.__version__,
+      packages=find_packages(),
+      include_package_data=True,
+      description=" An easy-to-use and colorful progress bar for python.",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author="K.y",
+      author_email="beidongjiedeguang@gmail.com",
+      url="https://github.com/beidongjiedeguang/python-progress-bar",
+      license="MIT",
+      install_requires=install_requires,
+      classifiers=[
+          'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: 3.7",
+          "Programming Language :: Python :: 3.8",
+      ],
+      keywords=[
+          'Python Utilities', 'Machine Learning'
+      ])
