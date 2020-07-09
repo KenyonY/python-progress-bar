4a9d67ab4db6b9d615275604b78f81852c8ded45
k.y <kunyuany@163.com>
2020-07-10 01:36:23 +0800

    add time_zone

--- pyprobar/progressbar.py
+++ pyprobar/progressbar.py
@@ -3,7 +3,7 @@ from .compose import *
 import time
 
 
-class probar():
+class probar:
     """Simple, easy to use, beautiful and fast progress bar.
 
     :arg color: options  'const_random', '0','1','2',...,'5'
@@ -21,11 +21,16 @@ class probar():
 
     >>> res  = [i for i in probar(range(10))]
     """
-
-    def __init__(self, iterable, total_steps=None, symbol_1="█", symbol_2='>',
-                 color='const_random',N_colors=4,
-                 enum = False,
+    def __init__(self,
+                 iterable,
+                 total_steps=None,
+                 symbol_1="█",
+                 symbol_2='>',
+                 color='const_random',
+                 N_colors=4,
+                 enum=False,
                  time_interval=0.02,
+                 time_zone=None,
                  terminal=False):
 
         self.iterable = iterable
@@ -36,19 +41,18 @@ class probar():
         self.color = color
         self.N_colors = N_colors
         self.enum = enum
-        self.total_steps = len(iterable) if total_steps is None else total_steps
+        self.total_steps = len(
+            iterable) if total_steps is None else total_steps
 
-
-        # self.q = Queue(2)
         self.q = deque(maxlen=1)
         self.q.append(0)
-        self.__threadbar = _Thread_probar(self.q, self.total_steps, time_interval,
-                              self.symbol_1, self.symbol_2,
-                              self.t0, self.color, self.N_colors, self.terminal)
-
-        # self.__threadbar.setDaemon(True)
+        self.__threadbar = _Thread_probar(self.q, self.total_steps,
+                                          time_interval, self.symbol_1,
+                                          self.symbol_2, self.t0, self.color,
+                                          self.N_colors, self.terminal, time_zone)
         self.__threadbar.start()
         self.isInterrupt = True
+
 
     def __iter__(self):
         try:
@@ -64,34 +68,35 @@ class probar():
 
 
 __threadbar = None
+
+
 @trydecorator2(__threadname=__threadbar)
-def bar(index, total_steps,
+def bar(index,
+        total_steps,
         color='const_random',
-        symbol_1="█", symbol_2='>',
+        symbol_1="█",
+        symbol_2='>',
         text='',
         time_interval=0.02,
+        time_zone=None,
         terminal=True,
-        q = deque(maxlen=1)):
+        q=deque(maxlen=1)):
     """Simple, easy to use, beautiful and fast progress bar.
-
+    Do not use it in jupyter notebook/lab or ipython kernel.
     :arg color: options  'const_random', '0','1','2',...,'5',
         or RGB a list, such as [250,205,229] or [[146,52,247],[250,205,229],[66,227,35],[214,126,209]]
-
     Examples
     --------
     >>> for idx, i in enumerate(range(1000)):
     >>>     bar(idx, 1000)
     >>>     ...
-
     >>> for idx, i in enumerate(iterable_x):
     >>>     bar(idx, len(iterable_x),text='I want show some texts in progress bar.')
     >>>     ...
-
     >>> N = len(iterable_x)
     >>> for idx, i in enumerate(iterable_x):
     >>>     bar(idx, N,color='const_random')
     >>>     ...
-
     >>> N = len(iterable_x)
     >>> for idx, i in enumerate(iterable_x):
     >>>     bar(idx, N,color = [[146,52,247],[250,205,229],[66,227,35],[214,126,209]])
@@ -99,17 +104,11 @@ def bar(index, total_steps,
     """
     global __threadbar
     _index = index + 1
-
-    if text == '':
-        q.append(_index)
-    else:
-        q.append((_index, text))
-
+    q.append((_index, text))
     if index == 0:
         t0 = time.time()
-        __threadbar = _Thread_bar(q, total_steps, time_interval,
-                                symbol_1, symbol_2,
-                                color, text, terminal,t0)
+        __threadbar = _Thread_bar(q, total_steps, time_interval, symbol_1,
+                                  symbol_2, color, text, terminal, t0,time_zone)
         __threadbar.setDaemon(True)
         __threadbar.start()
 
@@ -120,4 +119,3 @@ def bar(index, total_steps,
 
 if __name__ == "__main__":
     pass
-
